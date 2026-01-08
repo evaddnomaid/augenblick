003716b4d6edd457e968052e50907e6f4fbfa962
Dave Burchell <burchell@acm.org>
2026-01-07 22:34:32 -0600

    Starting on a cli bug edit util

---
+++ util/bz/cli_bz_edit.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+prettydate=`date +"%a %b %e, %Y"`
+echo $prettydate
+isodate=`date +"%Y-%m-%d"`
+echo $isodate
+cat >thistime <<ender
+{
+  "status" : "DIARY",
+  "product" : "DJB_Personal",
+  "component" : "diary",
+  "version" : "unspecified",
+  "summary" : "$prettydate",
+  "cf_revisit_date" : "$isodate"
+}
+ender
+
+# TODO: Get the token and IP addr from .config
+#echo "$THISDOC"
+#exit
+#curl -k -vv -X POST -d @thistime "https://xx.xx.xx.xx/bz/rest.cgi/bug?id=1&token=1-lxxxxxxxxx" --header "Content-Type: application/json"
+#curl -k -vv -X POST -d @thistime "https://xx.xx.xx.xx/bz/rest.cgi/bug?id=1&token=1-Oxxxxxxxxx" --header "Content-Type: application/json"
+curl -k -vv "https://xx.xx.xx.xx/bz/rest.cgi/bug?id=1&token=1-Oxxxxxxxxx" --header "Content-Type: application/json"
+# Call above uses a token, not a password.  To request a token:
+# curl -k "https://xx.xx.xx.xx/bz/rest.cgi/login?login=burchell@acm.org&password=xxxxxxxxxx"
+# Bug for this in Bugzilla is https://xx.xx.xx.xx/bz/show_bug.cgi?id=2076
