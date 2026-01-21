82cbf8e0b2cb1e97cbc2a3899e160ee8da367b26
Dave Burchell <burchell@acm.org>
2026-01-21 00:20:30 -0600

    using .config

--- util/bz/cli_bz_edit.sh
+++ util/bz/cli_bz_edit.sh
@@ -14,12 +14,21 @@ cat >thistime <<ender
 }
 ender
 
+source ./.config
+
+if [[ -n $bugid ]]; then
+	echo bugid set, using $bugid
+else
+	bugid=6000
+	echo bugid not set, using $bugid
+fi
+
 # TODO: Get the token and IP addr from .config
 #echo "$THISDOC"
 #exit
 #curl -k -vv -X POST -d @thistime "https://xx.xx.xx.xx/bz/rest.cgi/bug?id=1&token=1-lxxxxxxxxx" --header "Content-Type: application/json"
 #curl -k -vv -X POST -d @thistime "https://xx.xx.xx.xx/bz/rest.cgi/bug?id=1&token=1-Oxxxxxxxxx" --header "Content-Type: application/json"
-curl -k -vv "https://xx.xx.xx.xx/bz/rest.cgi/bug?id=1&token=1-Oxxxxxxxxx" --header "Content-Type: application/json"
+curl -k -vv "https://$bzhost/bz/rest.cgi/bug?id=$bugid&token=$bztoken" --header "Content-Type: application/json"
 # Call above uses a token, not a password.  To request a token:
 # curl -k "https://xx.xx.xx.xx/bz/rest.cgi/login?login=burchell@acm.org&password=xxxxxxxxxx"
 # Bug for this in Bugzilla is https://xx.xx.xx.xx/bz/show_bug.cgi?id=2076
